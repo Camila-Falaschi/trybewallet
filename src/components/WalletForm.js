462903c3bfe1cdccb04d2bf7f867861bdc055c24
Camila <falaschi.camila@yahoo.com.br>
2022-07-29 20:02:34 -0300

    colocando formulário de depesas no componente certo - WalletForm

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import SelectInput from './SelectIntput';
 
 export default class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <form>
+        <input type="number" min="0" data-testid="value-input" />
+        <SelectInput
+          testid="currency-input"
+          labelName="Moeda"
+          dataArray={ ['moeda'] }
+        />
+        <SelectInput
+          testid="method-input"
+          labelName="Método de Pagamento"
+          dataArray={ ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'] }
+        />
+        <SelectInput
+          testid="tag-input"
+          labelName="Tag:"
+          dataArray={ ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'] }
+        />
+        <input type="text" data-testid="description-input" />
+        <button type="button">Adicionar despesa</button>
+      </form>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import SelectInput from '../components/SelectIntput';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
@@ -9,26 +9,7 @@ class Wallet extends React.Component {
         <Header />
         <main>
           <section>
-            <form>
-              <input type="number" min="0" data-testid="value-input" />
-              <SelectInput
-                testid="currency-input"
-                labelName="Moeda"
-                dataArray={ ['moeda'] }
-              />
-              <SelectInput
-                testid="method-input"
-                labelName="Método de Pagamento"
-                dataArray={ ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'] }
-              />
-              <SelectInput
-                testid="tag-input"
-                labelName="Tag:"
-                dataArray={ ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'] }
-              />
-              <input type="text" data-testid="description-input" />
-              <button type="button">Adicionar despesa</button>
-            </form>
+            <WalletForm />
           </section>
         </main>
       </>
