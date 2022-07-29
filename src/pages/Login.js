43bc9019dd9900926d043cbea28379a27d59f677
Camila <falaschi.camila@yahoo.com.br>
2022-07-28 22:21:47 -0300

    criando validação do formulário de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,44 +6,63 @@ import { userAction } from '../redux/actions';
 class Login extends React.Component {
   constructor() {
     super();
-    this.state({
+    this.state = {
       email: '',
-      passsword: '',
+      password: '',
       isDisabled: true,
-    });
+    };
+  }
+
+  infoValidation = () => {
+    const { email, password } = this.state;
+    const minLength = 6;
+    return ((/\S+@\S+\.\S+/.test(email)) && (password.length >= minLength));
   }
 
   handleChange = ({ target }) => {
-    const { type, value } = target;
+    const { name, value } = target;
     this.setState({
-      [type]: value,
-    });
+      [name]: value,
+    }, () => (
+      (this.infoValidation())
+        ? (this.setState({
+          isDisabled: false,
+        }))
+        : (this.setState({
+          isDisabled: true,
+        }))
+    ));
   };
 
   handleSubmit = () => {
     const { email } = this.state;
-    const { dispatch } = this.props;
+    const { dispatch, history } = this.props;
     dispatch(userAction(email));
+    history.push('/carteira');
   };
 
   render() {
-    const { email, passsword, isDisabled } = this.state;
+    const { email, password, isDisabled } = this.state;
     return (
       <div>
         <h1>TrybeWallet</h1>
         <input
           type="email"
+          name="email"
           placeholder="e-mail"
           data-testid="email-input"
           value={ email }
           onChange={ this.handleChange }
+          required
         />
         <input
           type="password"
+          name="password"
           placeholder="senha"
           data-testid="password-input"
-          value={ passsword }
+          value={ password }
           onChange={ this.handleChange }
+          required
         />
         <button type="submit" disabled={ isDisabled } onClick={ this.handleSubmit }>
           Entrar
@@ -55,6 +74,9 @@ class Login extends React.Component {
 
 Login.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default connect()(Login);
