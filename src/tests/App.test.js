2d621c126b515ba617fa12ebac4e71b89bdef07c
Camila <falaschi.camila@yahoo.com.br>
2022-08-03 17:00:25 -0300

    criando testes

---
+++ src/tests/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from '../App';
+import { renderWithRouter } from './helpers/renderWith';
+
+describe('Login page', () => {
+  it('should render the correct path', () => {
+    renderWithRouter(<App />, ['/']);
+
+    expect(getByTestId);
+  });
+});
